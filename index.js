2a1d5c27526654b856101d9937c3288c0230e81a
Kartik <kartikmandhan14@gmail.com>
2020-09-05 16:25:35 +0530

    aos added

---
+++ index.js
@@ -0,0 +1,5 @@
+// var cards = document.getElementsByClassName("card-container");
+// for (var i = 0; i < cards.length; i++) {
+//   cards[i].setAttribute("data-aos", "zoom-in-up");
+//   cards[i].className += " aos-init aos-animate";
+// }
